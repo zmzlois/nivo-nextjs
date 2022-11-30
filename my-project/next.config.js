12a7e84ae1d36300d996d62cd23c77f877987b22
eliaskuhl <eliaskuhlmann@outlook.com>
2022-11-30 14:03:45 +0100

    Installed babel

--- my-project/next.config.js
+++ my-project/next.config.js
@@ -4,4 +4,28 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  // other configs...
+  reactStrictMode: true,
+  swcMinify: true,
+
+  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+            titleProp: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
